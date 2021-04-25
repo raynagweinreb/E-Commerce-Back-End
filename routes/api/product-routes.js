d558d2ac83e49f8cdad88b300591a3fd1a826c29
Rayna W <raynagweinreb@gmail.com>
2021-04-25 14:20:58 -0700

    make final changes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -60,20 +60,28 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      console.log(product)
+      if (req.body.tag_id) {
+        const productTagIdArr = req.body.tag_id.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
-      }
+      }else {
       // if no product tags, just respond
       res.status(200).json(product);
+      }
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
@@ -98,7 +106,7 @@ router.put('/:id', (req, res) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
+      const newProductTags = productTagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
@@ -108,7 +116,7 @@ router.put('/:id', (req, res) => {
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .filter(({ tag_id }) => !req.body.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
@@ -117,10 +125,10 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) => res.status(200).json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
+       console.log(err);
+      res.status(500).json(err);
     });
 });
 
@@ -138,7 +146,7 @@ router.delete('/:id', (req, res) => {
     res.json(prodResponse);
   }).catch(err =>{
     console.log(err);
-    res.status(500).jdon(err)
+    res.status(500).json(err)
   })
 });
 
